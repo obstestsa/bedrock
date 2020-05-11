c60e967b93c73638e9f1780f9953b0b18249d29d
Sajal Shrestha <sajal.shres@gmail.com>
2020-05-11 13:29:55 +0000

    Added test cases

---
+++ authenticate/tests/test_serializers.py
@@ -0,0 +1,110 @@
+"""Test Cases for auth-api serializers
+"""
+from django.contrib.auth.models import User
+from django.test import TestCase
+from rest_framework import exceptions
+from rest_framework_simplejwt.tokens import (
+    AccessToken, RefreshToken,
+)
+from authenticate.serializers import AuthTokenObtainPairSerializer
+
+
+class TestAuthTokenObtainPairSerializer(TestCase):
+    """
+    Tests for Token Obtain Serializer
+    """
+
+    def setUp(self):
+        self.username = "test_user"
+        self.password = "test_password"
+
+        self.user = User.objects.create_user(
+            username=self.username, password=self.password
+        )
+
+    def test_it_should_not_validate_if_any_fields_missing(self):
+        """
+        Should not validate when any of the fields is not providded
+        """
+        # empty data
+        serializer = AuthTokenObtainPairSerializer(data={})
+        self.assertFalse(serializer.is_valid())
+
+        # username only
+        serializer = AuthTokenObtainPairSerializer(
+            data={"username": "test_user_random"}
+        )
+        self.assertFalse(serializer.is_valid())
+        self.assertIn("password", serializer.errors)
+
+        # password only
+        serializer = AuthTokenObtainPairSerializer(
+            data={"password": "test_passwrod_random"}
+        )
+        self.assertFalse(serializer.is_valid())
+        self.assertIn("username", serializer.errors)
+
+    def test_it_should_not_validate_if_user_not_found(self):
+        """
+        Exception should be raised when user is not found
+        """
+        serializer = AuthTokenObtainPairSerializer(
+            data={
+                "username": "test_user_random",
+                "password": "test_password_random",
+            }
+        )
+
+        with self.assertRaises(exceptions.AuthenticationFailed):
+            serializer.is_valid()
+
+    def test_it_should_raise_if_user_not_active(self):
+        """
+        Exception should be raised when user is not active
+        """
+        self.user.is_active = False
+        self.user.save()
+
+        serializer = AuthTokenObtainPairSerializer(
+            data={"username": self.username, "password": self.password}
+        )
+
+        with self.assertRaises(exceptions.AuthenticationFailed):
+            serializer.is_valid()
+
+    def test_it_should_provide_a_token_when_data_valid(self):
+        """
+        Token should be provided with valid data
+        """
+        serializer = AuthTokenObtainPairSerializer(
+            data={"username": self.username, "password": self.password}
+        )
+
+        self.assertTrue(serializer.is_valid())
+        self.assertIn("access", serializer.validated_data)
+        self.assertIn("refresh", serializer.validated_data)
+
+    def test_it_should_provide_correct_token_subclass(self):
+        """
+        When token type is correct, intantiation of proper token subclass
+        should not raise exception
+        """
+        serializer = AuthTokenObtainPairSerializer(
+            data={"username": self.username, "password": self.password}
+        )
+
+        self.assertTrue(serializer.is_valid())
+
+        # Should raise TokenError if invalid token type
+        AccessToken(serializer.validated_data["access"])
+        RefreshToken(serializer.validated_data["refresh"])
+
+    def test_it_should_provide_extra_response_when_data_valid(self):
+        """
+        Extra responses should be provided with valid data
+        """
+        serializer = AuthTokenObtainPairSerializer(
+            data={"username": self.username, "password": self.password}
+        )
+
+        self.assertTrue(serializer.is_valid())
---
+++ authenticate/tests/test_views.py
@@ -0,0 +1,91 @@
+"""Test Cases for auth api views
+"""
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.state import token_backend
+
+from authenticate.tests.utils import APIViewTestCase
+
+
+class TestAuthTokenObtainPairView(APIViewTestCase):
+    """
+    Tests for Auth Token Obtain Pair View
+    """
+    view_name = 'token_obtain_pair'
+
+    def setUp(self):
+        self.username = 'test_username'
+        self.password = 'test_password'
+
+        self.user = User.objects.create_user(
+            username=self.username,
+            password=self.password
+        )
+
+    def test_it_should_return_400_if_any_fields_missing(self):
+        """
+        Tests for missing fields
+        """
+        # empty data
+        result = self.view_post(data={})
+        self.assertEqual(result.status_code, 400)
+        self.assertIn('username', result.data)
+        self.assertIn('password', result.data)
+
+        # username only
+        result = self.view_post(data={
+            'username': self.username
+        })
+        self.assertEqual(result.status_code, 400)
+        self.assertIn('password', result.data)
+
+        # password only
+        result = self.view_post(data={
+            'password': self.password
+        })
+        self.assertEqual(result.status_code, 400)
+        self.assertIn('username', result.data)
+
+    def test_it_should_return_401_if_credential_wrong(self):
+        """
+        Test for wrong credentials
+        """
+        result = self.view_post(data={
+            'username': self.username,
+            'password': 'test_password_wrong'
+        })
+        self.assertEqual(result.status_code, 401)
+        self.assertIn('detail', result.data)
+
+    def test_it_should_return_401_if_user_inactive(self):
+        """
+        Test when user is inactive
+        """
+        self.user.is_active = False
+        self.user.save()
+
+        result = self.view_post(data={
+            'username': self.username,
+            'password': self.password
+        })
+        self.assertEqual(result.status_code, 401)
+        self.assertIn('detail', result.data)
+
+    def test_it_should_validate_when_payload_correct(self):
+        """
+        Test when payload is okay
+        """
+        user_attributes = [
+            "username",
+            "email",
+        ]
+        result = self.view_post(data={
+            'username': self.username,
+            'password': self.password
+        })
+        self.assertEqual(result.status_code, 200)
+        self.assertIn('access', result.data)
+        self.assertIn('refresh', result.data)
+
+        payload = token_backend.decode(result.data['access'])
+        for attributes in user_attributes:
+            self.assertIn(attributes, payload)
